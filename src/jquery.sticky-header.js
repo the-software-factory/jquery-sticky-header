2a8f6c848e77e2cd6a18590b44dc31140a31e62f
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2015-08-24 19:06:23 +0200

    [[FEAT]] Code made more readable, Custom slot target and title implemented, remove visible elements added
    - Code made more readable and less redundant
    - Each sticky element can now specify a header slot and custom title to be shown when it hides due to scrolling
    - Remove header elements as they become visible again on scrolling down
    - Example buttons made bigger

--- src/jquery.sticky-header.js
+++ src/jquery.sticky-header.js
@@ -5,36 +5,52 @@ $(function() {
     var header = $(".sticky-header");
 
     $(window).scroll(function() {
+
       if(header.find("> .left > div").length === 0) {
         header.hide();
       }
+      else {
+        header.show();
+      }
+
+      function isHidden(item) {
+        var headerHeight = header.is(":visible") ? header.height() : 0;
+
+        return ($(window).scrollTop()  + headerHeight) >= ($(item).offset().top + $(item).height())
+      }
+
+      function inHeader(item) {
+        return header.find("[data-id=" + $(item).attr("id") + "]").length !== 0;
+      }
+
+      function removeFromHeader(item) {
+        header.find("[data-id=" + $(item).attr("id") + "]").remove();
+      }
+
+      function addToHeader(item) {
+        var options = JSON.parse($(item).attr("options"));
+        var slot = header.find(".left");
+
+        if (options.position === "L") {
+          slot = header.find(".left");
+        }
+        else if(options.position === "C") {
+          slot = header.find(".center");
+        }
+        else if(options.position === "R") {
+          slot = header.find(".right");
+        }
+
+        slot.append("<div data-id='" + $(item).attr("id") + "'>" + options.title + "</div>");
+      }
 
       $(".sticky").each(function() {
-        if (header.is(":visible")) {
-          // Sticky item was hidden
-          if (($(window).scrollTop()  + header.height()) >= ($(this).offset().top + $(this).height())) {
-            // If element wasn't appended yet
-            if (header.find("[data-id=" + $(this).attr("id") + "]").length === 0) {
-              //append it to header
-              header.find(".left").append("<div data-id='" + $(this).attr("id") + "'>" + $(this).attr("id") + "</div>");
-            }
-          } else {
-            if (header.find("[data-id=" + $(this).attr("id") + "]").length !== 0) {console.log("hey, there si an item to remove");
-              header.find("[data-id=" + $(this).attr("id") + "]").remove();
-            }
-          }
+        if (!isHidden(this) && inHeader(this)) {
+          removeFromHeader(this);
         }
-        // If header is hidden
-        else {
-          // if element was hidden
-          if ($(window).scrollTop() > ($(this).offset().top + $(this).height())) {
-            header.show();
-            if (header.find("[data-id=" + $(this).attr("id") + "]").length === 0) {
-              header.find(".left").append("<div data-id='" + $(this).attr("id") + "'>" + $(this).attr("id") + "</div>");
-            }
-          }
-          }
-
+        else if(isHidden(this) && !inHeader(this)) {
+          addToHeader(this);
+        }
       });
     });
 
